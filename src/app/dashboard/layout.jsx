134aeb5aa34eb6ef89fc017f14c33cbc4f5f1b9e
yeikoag <yeiko2003@gmail.com>
2023-10-19 23:21:58 -0600

    Prueba vercel

--- src/app/dashboard/layout.jsx
+++ src/app/dashboard/layout.jsx
@@ -8,15 +8,13 @@ export default function DashboardLayout({ children }) {
   const isSidebarOpen = true;
 
   return (
-    
     <div className="flex flex-col h-screen"> {/* Contenedor principal de columna */}
       <Navbar />
-      <div className="flex"> {/* Fila para Navbar y Contenido */}
+      <div className="flex"> {/* Fila para Navbar y Conteido */}
         <Navsidebar isOpen={isSidebarOpen} />
         <div className="flex-1 p-4 overflow-y-auto"><SessionAuthProvider>{children}</SessionAuthProvider></div> {/* Contenido principal */}
       </div>
     </div>
-    
   );
 }
 
