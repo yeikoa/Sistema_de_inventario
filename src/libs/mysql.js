2b26aaeb739298a1c88aebfa1f4e6714fa991831
yeikoag <yeiko2003@gmail.com>
2024-03-05 22:28:18 -0600

    Probar

--- src/libs/mysql.js
+++ src/libs/mysql.js
@@ -2,9 +2,9 @@ import mysql from 'serverless-mysql'
 
 export const conn = mysql({
     config:{
-        host: 'database-eligam.c4llwzqtfxcc.us-east-1.rds.amazonaws.com',
-        user: 'admin',
-        password: 'Duke2003',
+        host: '127.0.0.1',
+        user: 'root',
+        password: '',
         port: '3306',
         database:'Eligam'
     }
