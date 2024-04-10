69f3866183c4c4bc98f3efac19cd2ea3a87b14df
yeikoag <yeiko2003@gmail.com>
2024-04-10 10:12:47 -0600

    Probando

--- src/libs/mysql.js
+++ src/libs/mysql.js
@@ -2,9 +2,9 @@ import mysql from 'serverless-mysql'
 
 export const conn = mysql({
     config:{
-        host: '127.0.0.1',
-        user: 'root',
-        password: '',
+        host: 'eligam.chci4e62og5m.us-east-2.rds.amazonaws.com',
+        user: 'admin',
+        password: 'Eligam1234',
         port: '3306',
         database:'Eligam'
     }
