26c48fe677c7af684f511d069995a2782bdfa0d8
yeikoag <yeiko2003@gmail.com>
2024-04-11 11:44:51 -0600

    main designs

--- src/app/dashboard/components/dashboardC.jsx
+++ src/app/dashboard/components/dashboardC.jsx
@@ -1,35 +1,46 @@
-'use client'
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import { FaBox, FaTruck, FaRegListAlt, FaWarehouse, FaChartBar } from 'react-icons/fa';
-
+"use client";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import {
+  FaBox,
+  FaTruck,
+  FaRegListAlt,
+  FaWarehouse,
+  FaChartBar,
+} from "react-icons/fa";
+import { useRouter } from "next/navigation";
 const Dashboard = () => {
   const [totalProducts, setTotalProducts] = useState(null);
   const [totalProviders, setTotalProviders] = useState(null);
-
+  const [totalUsers, setTotalUsers] = useState(null);
+  const router = useRouter();
   useEffect(() => {
-    // Fetch the total number of products from your API
-    axios.get("/api/Inicio/productos")
-    .then((response) => {
-      const data = response.data; // Acceder a la propiedad 'data' de la respuesta
-      setTotalProducts(data.totalProducts);
-      console.log(data.totalProducts);
-    })
-    .catch((error) => {
-      console.error('Error fetching data:', error);
-    });
+    axios
+      .get("/api/Inicio/productos")
+      .then((response) => {
+        const data = response.data;
+        setTotalProducts(data.totalProducts);
+        //console.log(data.totalProducts);
+      })
+      .catch((error) => {
+        //console.error('Error fetching data:', error);
+      });
 
-    axios.get("/api/Inicio/proveedores")
-    .then((response) => {
-      const data = response.data; // Acceder a la propiedad 'data' de la respuesta
-      setTotalProviders(data.totalProviders);
-      console.log(data.totalProviders);
-    })
-    .catch((error) => {
-      console.error('Error fetching data:', error);
+    axios
+      .get("/api/Inicio/proveedores")
+      .then((response) => {
+        const data = response.data;
+        setTotalProviders(data.totalProviders);
+        //console.log(data.totalProviders);
+      })
+      .catch((error) => {
+        // console.error('Error fetching data:', error);
+      });
+    axios.get("/api/Inicio/usuarios-total").then((response) => {
+      const data = response.data;
+      setTotalUsers(data.totalUsers);
+      //console.log(data.totalUsers);
     });
-  
-
   }, []);
 
   return (
@@ -63,28 +74,29 @@ const Dashboard = () => {
         </div>
         <div className="bg-white p-6 rounded-lg shadow-lg">
           <div className="flex items-center space-x-2">
+            <FaBox className="text-blue-600 text-3xl" />
+            <div>
+              <h3 className="text-xl font-bold">Usuarios activos</h3>
+              {totalUsers !== null ? (
+                <p className="text-gray-600">{totalUsers} Usuarios</p>
+              ) : (
+                <p className="text-gray-600">Cargando...</p>
+              )}
+            </div>
+          </div>
+        </div>
+
+        <div className="bg-white p-6 rounded-lg shadow-lg">
+          <div className="flex items-center space-x-2">
             <FaRegListAlt className="text-blue-600 text-3xl" />
             <div>
-              <h3 className="text-xl font-bold">Reportes de Inventario</h3>
-              <p className="text-gray-600">Ver Reportes</p>
-            </div>
-          </div>
-        </div>
-        <div className="bg-white p-6 rounded-lg shadow-lg">
-          <div className="flex items-center space-x-2">
-            <FaWarehouse className="text-blue-600 text-3xl" />
-            <div>
-              <h3 className="text-xl font-bold">Bajos en Stock</h3>
-              <p className="text-gray-600">20 Productos</p>
-            </div>
-          </div>
-        </div>
-        <div className="bg-white p-6 rounded-lg shadow-lg">
-          <div className="flex items-center space-x-2">
-            <FaChartBar className="text-blue-600 text-3xl" />
-            <div>
-              <h3 className="text-xl font-bold">Productos con Mayor Cantidad</h3>
-              <p className="text-gray-600">Ver Lista</p>
+              <h3 className="text-xl font-bold">Ver Inventario</h3>
+              <button
+                onClick={() => router.push("/dashboard/repuestosAlmacen")}
+                className="text-gray-600"
+              >
+                Ver Lista
+              </button>
             </div>
           </div>
         </div>
@@ -92,8 +104,13 @@ const Dashboard = () => {
           <div className="flex items-center space-x-2">
             <FaChartBar className="text-blue-600 text-3xl" />
             <div>
-              <h3 className="text-xl font-bold">Reportes de Proveedores</h3>
-              <p className="text-gray-600">Ver Lista</p>
+              <h3 className="text-xl font-bold">Ver Proveedores</h3>
+              <button
+                onClick={() => router.push("/dashboard/proveedoresTotal")}
+                className="text-gray-600"
+              >
+                Ver Lista
+              </button>
             </div>
           </div>
         </div>
@@ -102,7 +119,12 @@ const Dashboard = () => {
             <FaChartBar className="text-blue-600 text-3xl" />
             <div>
               <h3 className="text-xl font-bold">Historial Facturas</h3>
-              <p className="text-gray-600">Ver Lista</p>
+              <button
+                onClick={() => router.push("/dashboard/movimientos/facturas")}
+                className="text-gray-600"
+              >
+                Ver Lista
+              </button>
             </div>
           </div>
         </div>
@@ -111,7 +133,12 @@ const Dashboard = () => {
             <FaChartBar className="text-blue-600 text-3xl" />
             <div>
               <h3 className="text-xl font-bold">Usuarios Registrados</h3>
-              <p className="text-gray-600">Ver Lista</p>
+              <button
+                onClick={() => router.push("/dashboard/usuarios")}
+                className="text-gray-600"
+              >
+                Ver Lista
+              </button>
             </div>
           </div>
         </div>
