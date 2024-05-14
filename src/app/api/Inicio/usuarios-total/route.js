d3d740e707be24930668c06782fc21b66e852a6d
yeikoag <yeiko2003@gmail.com>
2024-05-13 22:40:50 -0600

    home update complete

--- src/app/api/Inicio/usuarios-total/route.js
+++ src/app/api/Inicio/usuarios-total/route.js
@@ -24,3 +24,33 @@ export async function GET() {
     );
   }
 }
+export async function POST(request) {
+  try {
+    const { nombre_usuario, password, nombre_completo, email } =
+      await request.json();
+
+    const result = await conn.query(
+      "INSERT INTO Usuarios (nombre_usuario, password, nombre_completo, email) VALUES (?, ?, ?, ?)",
+      [nombre_usuario, password, nombre_completo, email]
+    );
+
+    return NextResponse.json({
+      nombre_usuario,
+      nombre_completo,
+      email,
+      //estado,
+      id: result.insertId,
+    });
+  } catch (error) {
+    // Manejar errores
+    //console.error(error);
+    return NextResponse.json(
+      {
+        message: error.message,
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+}
